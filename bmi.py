5dce25f9f68b3bb4b5040af7cd416c606f8fa81e
Catriona Murray <g00063047@gmit.ie>
2022-03-13 14:54:41 +0000

    Week 2 - task 2

--- bmi.py
+++ bmi.py
@@ -1,12 +1,13 @@
 # bmi.py
 # This program calculates BMI inputted by user
 # author: Catriona Murray
-weight = float(input("Enter Weight(kg):"))
-height = float(input("Enter Height (cm):"))
-heightm = float(height/100)
-bmi = round(weight/(heightm**2),2)
-print("The BMI is (kg/m2) " +str(bmi) +".")
-if bmi < 18.5:
+from pickletools import int4 #Import Library
+weight = int(input("Enter Weight(kg):")) # Take in weight as integer
+height = int(input("Enter Height (cm):"))  #Take in height as integer
+heightm = float(height/100) # Convert tp meters
+bmi = round(weight/(heightm**2),2) # calculate BMI and round to rtwo decimal place
+print("The BMI is (kg/m2) " +str(bmi) +".") # Output BMI as a strong
+if bmi < 18.5: # Output the classification of BMI using of if statement
   print("Your BMI is classed as underweight")
 elif bmi < 25:
   print("Your BMI is classed as normal")
