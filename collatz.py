3804a5fcda87e5b53f32c9f078fc414910b1a305
Catriona Murray <g00063047@gmit.ie>
2022-03-13 16:02:08 +0000

    Week 4 - task 4

--- collatz.py
+++ collatz.py
@@ -3,16 +3,13 @@
 # This program takes in an absolute integer, checks if it is even, if it is even it divides number by 2 otherwise it multiplys by 3 and adds 1
 def collatz(num): #Define a function that passes variable of type integer
     while num != 1: #Do the below steps as long until it doesnt reach 1
-        print(num) #print out the num variable value
+        print(num , end=" ") #print out the num variable value
         if num % 2 == 0: #If the num variable value is even so divide by 2
             num = int(num / 2)
         else:
             num = int(3 * num + 1)  #Otherwise the num variable value is odd so divide by 2
     else:
-        print(num) #if the number variable reaches 1 end while
-        print('Number 1 was reached!')
-
-
+        print(num , end =" ") #if the number variable reaches 1 end while
 def main():
     n = int(input('Please Input a positive integer enter 1 to exit: ')) # Define a function that takes absolute integer from user and retuns that integer to defined collatz function.
     num =abs(n) # ensure its a positive number
