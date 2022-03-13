b855eada6dd64afb66625a6a3abe588082f9a8fb
Catriona Murray <g00063047@gmit.ie>
2022-03-13 15:36:54 +0000

    Week 2 - task 2

--- bmi.py
+++ bmi.py
@@ -4,14 +4,10 @@
 from pickletools import int4 #Import Library
 weight = int(input("Enter Weight(kg):")) # Take in weight as integer
 height = int(input("Enter Height (cm):"))  #Take in height as integer
-heightm = float(height/100) # Convert tp meters
-bmi = round(weight/(heightm**2),2) # calculate BMI and round to rtwo decimal place
-print("The BMI is (kg/m2) " +str(bmi) +".") # Output BMI as a strong
-if bmi < 18.5: # Output the classification of BMI using of if statement
-  print("Your BMI is classed as underweight")
-elif bmi < 25:
-  print("Your BMI is classed as normal")
-elif bmi < 30:
-  print("Your BMI is classed as overweight")
-else:
-  print("Your BMI is classed as obese")
+heightm = float(height/100) # Convert to meters
+bmi = round((weight/(heightm**2)),2) # calculate BMI and round to two decimal place
+print("The BMI is (kg/m2) " +str(bmi) +".") # Output BMI as a string
+if bmi < 18.5: print("Your BMI is classed as underweight",end=' ') #output classification of BMI without printing blank line
+elif bmi < 25: print("Your BMI is classed as normal",end=' ')
+elif bmi < 30: print("Your BMI is classed as overweight",end=' ')
+else: print("Your BMI is classed as obese",end=' ')
