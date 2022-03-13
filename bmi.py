410668c9c5b8b5df66c61c9d08d73dc386969f38
Catriona Murray <g00063047@gmit.ie>
2022-03-13 18:50:08 +0000

    Week 2 - task 2

--- bmi.py
+++ bmi.py
@@ -9,5 +9,5 @@ bmi = round((weight/(heightm**2)),2) # calculate BMI and round to two decimal pl
 print("The BMI is (kg/m2) " +str(bmi) +".") # Output BMI as a string
 if bmi < 18.5: print("Your BMI is classed as underweight",end=' ') #output classification of BMI without printing blank line
 elif bmi < 25: print("Your BMI is classed as normal",end=' ')
-elif bmi < 30: print("Your BMI is classed as overweight",end=' ')
-else: print("Your BMI is classed as obese",end=' ')
+elif bmi < 30: print("Your BMI is classed as overweight",end=' ') 
+else: print("Your BMI is classed as obese",end=' ') #end='' prevent print out of new line
