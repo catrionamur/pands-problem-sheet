347a273e97433917bc9035176b8771c8f80b3256
Catriona Murray <g00063047@gmit.ie>
2022-04-03 21:29:06 +0100

    Week 7 - Task 7

--- es.py
+++ es.py
@@ -1,10 +1,9 @@
 # plottask.py
 # This program reads in a text file which is passed as an argument from a user and calculates number of e's
 # Author: Catriona Murray
-
 import sys # Import library
 fileName = sys.argv[1] # Use function from predefined sys lbrary which expects return of one parameter and store in variable filename
-inputFile = open(fileName, "r")  # open file in read mode
+inputFile = open(fileName, "r",encoding="utf8")  # open file in read mode , to avoid 'charmap' codec can't decode byte error add encoding of txt file
 def letterFrequency (fileName, letter): #define function that has 2 arguments, name of the file and the letter that is being checked 
     # store content of the file in a variable for processing
     text = inputFile.read()
@@ -13,7 +12,7 @@ def letterFrequency (fileName, letter): #define function that has 2 arguments, n
     # iterate through each character in the text filer
     for char in text: 
         # compare each character with the given letter
-        if char == letter:
+        if char == 'e' or 'E': #(check for lower and uppercase e)
             count += 1 # If the letter is found iterate count by 1
     # return letter count 
     return count
