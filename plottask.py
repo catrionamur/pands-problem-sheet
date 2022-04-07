c499e91db14ed69bdf4dc1c8929c49b0691244e1
Catriona Murray <g00063047@gmit.ie>
2022-04-07 21:10:56 +0100

    Week 8 - Task 8

--- plottask.py
+++ plottask.py
@@ -6,10 +6,11 @@ import numpy as np #import numpy to use array
 range = np.array(range(0,4))
 gpoints = range * range #multiply each entry by itself range squared
 hpoints = range * range * range #range cubed
-plt.style.use('dark_background') #set the color of the background to plack
+plt.style.use('dark_background') #set the color of the background to Black
 plt.plot(gpoints, hpoints, color='#aa77cc', label = "f(x)=x" ) #plot graph set color of line to purple and set the label
+plt.grid(True) #add grid lines
 plt.title("plottask.py",size=18) #set the title and set the font of the title to 18
 plt.xlabel("$g(x)=_{x^2}$", size=16) #Set x axis label with x squared in italic
 plt.ylabel("$h(x)=_{x^3}$",size=16) #Set y axis label with y in italic
 plt.legend(loc ='upper center') #Place the legend in the center
-plt.show()
+plt.show() #show plot
